220840b5981d729aae3e0abac238d90511a98114
shm_usuf <shmusuf@gmail.com>
2023-02-21 20:16:47 +0600

    nav & hero added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,10 @@
 const Footer = () => {
   return (
-    <div>
-      Footer
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae officia
-        quaerat, similique fuga soluta voluptates voluptatem reprehenderit omnis
-        dolore! Quidem velit nemo ut provident laboriosam autem, obcaecati
-        facere! Similique labore sequi natus non amet exercitationem laborum
-        dignissimos odit minus tempore cupiditate doloremque, illum aut sed quam
-        blanditiis vero. Fuga consequatur possimus recusandae, eum dolore enim
-        obcaecati eaque repudiandae sequi illum perspiciatis ratione laborum,
-        tempore maxime eligendi cumque laboriosam, impedit vitae exercitationem
-        magnam ducimus nemo quibusdam minima. Repellendus perferendis eius
-        reiciendis doloribus vel ut qui pariatur, necessitatibus, laboriosam,
-        libero cupiditate debitis incidunt enim aliquid tempore beatae
-        praesentium magni culpa quo animi eaque. Unde, commodi numquam. Nesciunt
-        totam ab porro quae maxime rerum voluptas fugiat similique. Ullam, sed.
-        Consequuntur velit eius voluptates, explicabo, nemo minus delectus sed
-        neque exercitationem rerum est perspiciatis eum fugit totam temporibus
-        nihil repellat accusantium ipsum voluptatum. Laborum, culpa. Quia,
-        quaerat at blanditiis atque, corrupti id maiores nam veritatis sapiente
-        natus facere obcaecati eos. Laborum amet animi vero totam hic distinctio
-        quo commodi. Dolorem, voluptas ipsum? Explicabo, ut est minus quam ex
-        voluptatem eum qui unde atque animi repudiandae, iure ullam dolores
-        perspiciatis magni sint odio dolorum libero fugiat! Obcaecati ad modi
-        ullam, distinctio sunt et id. Animi, facere illum nesciunt impedit
-        consequuntur sed quis modi explicabo repellat deleniti ratione fuga
-        voluptates, laborum ipsum, veritatis minus quos aperiam. Quam, nostrum
-        illum magni cumque deserunt labore numquam sequi, nulla culpa ex ad
-        consequatur, voluptates quisquam iste suscipit iusto eos doloribus?
-        Minima aliquam impedit est? Corrupti, fuga tempore! Accusamus, nesciunt.
-      </p>
+    <div className="footer py-10 capitalize text-xl">
+      <div className="footer-container container mx-auto flex justify-between items-center">
+        <p>copyright &copy;{new Date().getFullYear()} laptop bazar.</p>
+        <p>powered by shm usuf</p>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,40 +2,39 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className="header bg-gray-700-700 bg-transparent">
-      <nav className="navbar container mx-auto capitalize flex justify-between items-center h-20">
-        <div className="logo cursor-pointer text-lg text-gray-700">
+    <header className="header h-20 flex capitalize">
+      <nav className="nav container mx-auto flex justify-between items-center">
+        <div className="logo cursor-pointer text-gray-600 text-lg">
           <span>navbar logo</span>
         </div>
 
-        <ul className="links flex gap-5 text-lg text-gray-700">
+        <ul className="links text-lg text-gray-600 flex justify-between items-center gap-5">
           <li className="">
-            <Link className="link-item ">Home</Link>
+            <Link className="link-item ">home</Link>
           </li>
           <li>
-            <Link className="link-item">Store</Link>
+            <Link className="link-item">store</Link>
           </li>
           <li>
-            <Link className="link-item">Men</Link>
+            <Link className="link-item">men</Link>
           </li>
           <li>
-            <Link className="link-item">Women</Link>
+            <Link className="link-item">women</Link>
           </li>
           <li>
-            <Link className="link-item">Accessories</Link>
+            <Link className="link-item">accessories</Link>
           </li>
           <li>
-            <Link className="link-item">About</Link>
+            <Link className="link-item">about</Link>
           </li>
           <li>
-            <Link className="link-item">Account</Link>
+            <Link className="link-item">account</Link>
           </li>
           <li>
-            <Link className="link-item">Contact</Link>
+            <Link className="link-item">contact</Link>
           </li>
         </ul>
       </nav>
-      <div className="acar-icon"></div>
     </header>
   );
 };
