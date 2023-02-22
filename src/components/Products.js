808172669d614d54f1830753ee3031ad9f8a9b77
shm_usuf <shmusuf@gmail.com>
2023-02-22 18:39:19 +0600

    excercise purpose

---
+++ src/components/Products.js
@@ -0,0 +1,165 @@
+const Products = () => {
+  return (
+    <div className="our-products mt-40">
+      <h2 className="product-title relative text-6xl text-center mb-20">
+        Our products
+      </h2>
+      <div className="product-wrapper mt-40 grid grid-cols-5 gap-5 px-10">
+        <div className="product-cart w-60 overflow-hidden  px-2 border">
+          <div className="product-img w-full">
+            <img
+              src="https://skyblue.com.bd/wp-content/uploads/2020/09/Black-Fbd_Westhighland-Whit_1-300x300.jpg"
+              alt="t-shirt"
+            />
+          </div>
+          <div className="product-text flex flex-col">
+            <h3 className="text-gray-900">
+              Black Man's Full Sleeve L/S Shirt.
+            </h3>
+            <span className="text-gray-400">men</span>
+            <span className="text-gray-900">
+              <span className="font-bold text-lg">৳</span>1,290.00
+            </span>
+          </div>
+        </div>
+        <div className="product-cart w-60 overflow-hidden flex flex-col gap-5 px-2 border">
+          <div className="product-img w-full">
+            <img
+              src="https://skyblue.com.bd/wp-content/uploads/2020/09/Black-Fbd_Westhighland-Whit_1-300x300.jpg"
+              alt="t-shirt"
+            />
+          </div>
+          <div className="product-text flex flex-col">
+            <h3 className="text-gray-900">
+              Black Man's Full Sleeve L/S Shirt.
+            </h3>
+            <span className="text-gray-400">men</span>
+            <span className="text-gray-900">৳ 1,290.00</span>
+          </div>
+        </div>
+        <div className="product-cart w-60 overflow-hidden flex flex-col gap-5 px-2 border">
+          <div className="product-img w-full">
+            <img
+              src="https://skyblue.com.bd/wp-content/uploads/2020/09/Black-Fbd_Westhighland-Whit_1-300x300.jpg"
+              alt="t-shirt"
+            />
+          </div>
+          <div className="product-text flex flex-col">
+            <h3 className="text-gray-900">
+              Black Man's Full Sleeve L/S Shirt.
+            </h3>
+            <span className="text-gray-400">men</span>
+            <span className="text-gray-900">৳ 1,290.00</span>
+          </div>
+        </div>
+        <div className="product-cart w-60 overflow-hidden flex flex-col gap-5 px-2 border">
+          <div className="product-img w-full">
+            <img
+              src="https://skyblue.com.bd/wp-content/uploads/2020/09/Black-Fbd_Westhighland-Whit_1-300x300.jpg"
+              alt="t-shirt"
+            />
+          </div>
+          <div className="product-text flex flex-col">
+            <h3 className="text-gray-900">
+              Black Man's Full Sleeve L/S Shirt.
+            </h3>
+            <span className="text-gray-400">men</span>
+            <span className="text-gray-900">৳ 1,290.00</span>
+          </div>
+        </div>
+        <div className="product-cart w-60 overflow-hidden flex flex-col gap-5 px-2 border">
+          <div className="product-img w-full">
+            <img
+              src="https://skyblue.com.bd/wp-content/uploads/2020/09/Black-Fbd_Westhighland-Whit_1-300x300.jpg"
+              alt="t-shirt"
+            />
+          </div>
+          <div className="product-text flex flex-col">
+            <h3 className="text-gray-900">
+              Black Man's Full Sleeve L/S Shirt.
+            </h3>
+            <span className="text-gray-400">men</span>
+            <span className="text-gray-900">৳ 1,290.00</span>
+          </div>
+        </div>
+        <div className="product-cart w-60 overflow-hidden flex flex-col gap-5 px-2 border">
+          <div className="product-img w-full">
+            <img
+              src="https://skyblue.com.bd/wp-content/uploads/2020/09/Black-Fbd_Westhighland-Whit_1-300x300.jpg"
+              alt="t-shirt"
+            />
+          </div>
+          <div className="product-text flex flex-col">
+            <h3 className="text-gray-900">
+              Black Man's Full Sleeve L/S Shirt.
+            </h3>
+            <span className="text-gray-400">men</span>
+            <span className="text-gray-900">৳ 1,290.00</span>
+          </div>
+        </div>
+        <div className="product-cart w-60 overflow-hidden flex flex-col gap-5 px-2 border">
+          <div className="product-img w-full">
+            <img
+              src="https://skyblue.com.bd/wp-content/uploads/2020/09/Black-Fbd_Westhighland-Whit_1-300x300.jpg"
+              alt="t-shirt"
+            />
+          </div>
+          <div className="product-text flex flex-col">
+            <h3 className="text-gray-900">
+              Black Man's Full Sleeve L/S Shirt.
+            </h3>
+            <span className="text-gray-400">men</span>
+            <span className="text-gray-900">৳ 1,290.00</span>
+          </div>
+        </div>
+        <div className="product-cart w-60 overflow-hidden flex flex-col gap-5 px-2 border">
+          <div className="product-img w-full">
+            <img
+              src="https://skyblue.com.bd/wp-content/uploads/2020/09/Black-Fbd_Westhighland-Whit_1-300x300.jpg"
+              alt="t-shirt"
+            />
+          </div>
+          <div className="product-text flex flex-col">
+            <h3 className="text-gray-900">
+              Black Man's Full Sleeve L/S Shirt.
+            </h3>
+            <span className="text-gray-400">men</span>
+            <span className="text-gray-900">৳ 1,290.00</span>
+          </div>
+        </div>
+        <div className="product-cart w-60 overflow-hidden flex flex-col gap-5 px-2 border">
+          <div className="product-img w-full">
+            <img
+              src="https://skyblue.com.bd/wp-content/uploads/2020/09/Black-Fbd_Westhighland-Whit_1-300x300.jpg"
+              alt="t-shirt"
+            />
+          </div>
+          <div className="product-text flex flex-col">
+            <h3 className="text-gray-900">
+              Black Man's Full Sleeve L/S Shirt.
+            </h3>
+            <span className="text-gray-400">men</span>
+            <span className="text-gray-900">৳ 1,290.00</span>
+          </div>
+        </div>
+        <div className="product-cart w-60 overflow-hidden flex flex-col gap-5 px-2 border">
+          <div className="product-img w-full">
+            <img
+              src="https://skyblue.com.bd/wp-content/uploads/2020/09/Black-Fbd_Westhighland-Whit_1-300x300.jpg"
+              alt="t-shirt"
+            />
+          </div>
+          <div className="product-text flex flex-col">
+            <h3 className="text-gray-900">
+              Black Man's Full Sleeve L/S Shirt.
+            </h3>
+            <span className="text-gray-400">men</span>
+            <span className="text-gray-900">৳ 1,290.00</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import Feature from "../components/Feature";
 import Hero from "../components/Hero";
+import Products from "../components/Products";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Feature />
+      <Products />
     </div>
   );
 };
